44ac16339acfae076ff3e892f6ab99675a880a01
Kenye <kjm46384@uga.edu>
2022-03-03 15:06:31 -0500

    commit on 'main'

--- index.php
+++ index.php
@@ -12,13 +12,13 @@
     // When form submitted, check and create user session.
     if (isset($_POST['username'])) {
         $username = stripslashes($_REQUEST['username']);    // removes backslashes
-        $username = mysqli_real_escape_string($con, $username);
+        $username = mysqli_real_escape_string($conn, $username);
         $password = stripslashes($_REQUEST['password']);
-        $password = mysqli_real_escape_string($con, $password);
+        $password = mysqli_real_escape_string($conn, $password);
         // Check user is exist in the database
         $query    = "SELECT * FROM `users` WHERE username='$username'
                      AND password='" . md5($password) . "'";
-        $result = mysqli_query($con, $query) or die(mysqli_error($con));
+        $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
         $rows = mysqli_num_rows($result);
         if ($rows == 1) {
             $_SESSION['username'] = $username;
